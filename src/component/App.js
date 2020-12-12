84e5a50ae6b436bbacdebffc2c292cd25bb0c0eb
alapatbh <44811890+alapatbh@users.noreply.github.com>
2020-12-12 16:41:28 -0500

    ✨ Added document listers for the notifications

--- src/component/App.js
+++ src/component/App.js
@@ -1,26 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Display from "./Display";
 import ButtonPanel from "./ButtonPanel";
 import calculate from "../logic/calculate";
 import "./App.css";
 
-export default class App extends React.Component {
-  state = {
-    total: null,
-    next: null,
-    operation: null,
-  };
+export default props => {
+  const [notification, setNotification] = useState("");
+  window.addEventListener('fcmNotiification', (e) => {
+    let pinpointObj = e.detail;
+    console.log(pinpointObj);
+    setNotification(pinpointObj["pinpoint.notification.body"]);
+  });
 
-  handleClick = buttonName => {
-    this.setState(calculate(this.state, buttonName));
-  };
-
-  render() {
-    return (
-      <div className="component-app">
-        <Display value={this.state.next || this.state.total || "0"} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </div>
-    );
-  }
+  return (
+    <div className="component-app">
+      <h2>FCM pinpoint integration</h2>
+      <p>{notification}</p>
+    </div>
+  );
 }
